bdb92c4475742d5b75705d5143d0b59bc79a7f8a
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2020-01-31 17:17:18 -0500

    Add BottomNavigationView and some style

--- app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/MainActivity.kt
+++ app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.sonphil.canadarecallsandsafetyalerts
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar)
+    }
 }
