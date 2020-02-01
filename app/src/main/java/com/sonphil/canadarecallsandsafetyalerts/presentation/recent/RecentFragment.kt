67a89515136b1f27364a2fcef63b72e8a6bfd003
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2020-02-01 15:36:07 -0500

    Add basic implementation of RecentFragment

--- app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/presentation/recent/RecentFragment.kt
+++ app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/presentation/recent/RecentFragment.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.sonphil.canadarecallsandsafetyalerts.R
+import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
-class RecentFragment : Fragment() {
+class RecentFragment : DaggerFragment() {
+
+    private val viewModel: RecentViewModel by lazy {
+        ViewModelProvider(this, viewModelFactory).get(RecentViewModel::class.java)
+    }
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +25,22 @@ class RecentFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_recent, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        subscribeUI()
+    }
+
+    private fun subscribeUI() {
+        viewModel.recentRecalls.observe(viewLifecycleOwner, Observer {
+
+        })
+
+        viewModel.loading.observe(viewLifecycleOwner, Observer {
+        })
+
+        viewModel.error.observe(viewLifecycleOwner, Observer {
+            
+        })
+    }
 }
