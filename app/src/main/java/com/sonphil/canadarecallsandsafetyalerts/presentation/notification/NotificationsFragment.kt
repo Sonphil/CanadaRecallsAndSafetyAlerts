226af6d406ad6ba5db19995a1fd2ec1ae2fff9e8
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2020-02-08 01:20:15 -0500

    Add button to navigate to notifications settings

--- app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/presentation/notification/NotificationsFragment.kt
+++ app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/presentation/notification/NotificationsFragment.kt
@@ -1,14 +1,22 @@
 package com.sonphil.canadarecallsandsafetyalerts.presentation.notification
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.provider.Settings
+import android.view.*
 import com.sonphil.canadarecallsandsafetyalerts.R
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class NotificationsFragment : DaggerFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,5 +30,30 @@ class NotificationsFragment : DaggerFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         requireActivity().iv_section_icon.setImageResource(R.drawable.ic_notifications_black_24dp)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_notifications, menu)
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_notifications_settings -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                    intent.putExtra(Settings.EXTRA_APP_PACKAGE, requireContext().packageName)
+                    startActivity(intent)
+                } else {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    intent.data = Uri.parse("package:${requireContext().packageName}")
+                    startActivity(intent)
+                }
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
