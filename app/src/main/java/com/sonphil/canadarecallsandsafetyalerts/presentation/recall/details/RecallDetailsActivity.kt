4eac759f1f76a303e640275eb59c2b05e899101f
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2020-02-10 22:52:57 -0500

    Set status bar to translucent in RecallDetailsActivity

--- app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/presentation/recall/details/RecallDetailsActivity.kt
+++ app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/presentation/recall/details/RecallDetailsActivity.kt
@@ -1,6 +1,9 @@
 package com.sonphil.canadarecallsandsafetyalerts.presentation.recall.details
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import com.sonphil.canadarecallsandsafetyalerts.R
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_recall_details.*
@@ -20,6 +23,30 @@ class RecallDetailsActivity : DaggerAppCompatActivity() {
             finish()
         }
 
+        setupWindow()
         btn_back.setOnClickListener { onBackPressed() }
+    }
+
+    private fun setupWindow() {
+        root.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+        )
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        addInsetTopToBackButtonTopMargin()
+    }
+
+    private fun addInsetTopToBackButtonTopMargin() {
+        val params = btn_back.layoutParams as ViewGroup.MarginLayoutParams
+
+        params.topMargin = params.topMargin + window.decorView.rootWindowInsets.stableInsetTop
+        btn_back.layoutParams = params
     }
 }
