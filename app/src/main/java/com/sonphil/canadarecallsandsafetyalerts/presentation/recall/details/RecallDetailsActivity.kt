3927483a56ac7c646abde7c3d626603155284165
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2020-02-10 22:09:06 -0500

    Add AppBarLayout and back button

--- app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/presentation/recall/details/RecallDetailsActivity.kt
+++ app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/presentation/recall/details/RecallDetailsActivity.kt
@@ -3,17 +3,23 @@ package com.sonphil.canadarecallsandsafetyalerts.presentation.recall.details
 import android.os.Bundle
 import com.sonphil.canadarecallsandsafetyalerts.R
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_recall_details.*
 
 class RecallDetailsActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_recall_details)
 
-        intent?.extras ?.let { bundle ->
-            val recall = RecallDetailsActivityArgs.fromBundle(bundle).recall
+        val recall = intent?.extras?.run {
+            RecallDetailsActivityArgs.fromBundle(this).recall
         }
+
+        if (recall == null) {
+            finish()
+        }
+
+        btn_back.setOnClickListener { onBackPressed() }
     }
 }
