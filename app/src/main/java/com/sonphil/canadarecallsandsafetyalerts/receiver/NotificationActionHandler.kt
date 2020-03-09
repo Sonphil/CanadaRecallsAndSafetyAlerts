bfa40420cae81dff0764a81cecdc85c5e98f025f
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2020-03-08 20:54:10 -0400

    Add NotificationActionHandler which will handle actions for NotificationActionReceiver

---
+++ app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/receiver/NotificationActionHandler.kt
@@ -0,0 +1,51 @@
+package com.sonphil.canadarecallsandsafetyalerts.receiver
+
+import android.content.Context
+import androidx.annotation.StringRes
+import com.sonphil.canadarecallsandsafetyalerts.R
+import com.sonphil.canadarecallsandsafetyalerts.entity.Recall
+import com.sonphil.canadarecallsandsafetyalerts.ext.toast
+import com.sonphil.canadarecallsandsafetyalerts.repository.BookmarkRepository
+import com.sonphil.canadarecallsandsafetyalerts.repository.ReadStatusRepository
+import com.sonphil.canadarecallsandsafetyalerts.utils.NotificationsUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Created by Sonphil on 08-03-20.
+ */
+
+@Singleton
+class NotificationActionHandler @Inject constructor(
+    private val notificationUtils: NotificationsUtils,
+    private val bookmarkRepository: BookmarkRepository,
+    private val readStatusRepository: ReadStatusRepository
+) {
+    fun handleRecallAction(context: Context, recall: Recall?, action: String?) {
+        if (recall != null) {
+            notificationUtils.dismissNotification(context, recall)
+
+            GlobalScope.launch(Dispatchers.IO) {
+                if (action == context.getString(R.string.action_mark_recall_as_read)) {
+                    readStatusRepository.markRecallAsRead(recall)
+
+                    showSuccessMessage(context, R.string.message_recall_marked_as_read)
+                } else if (action == context.getString(R.string.action_bookmark_recall)) {
+                    bookmarkRepository.updateBookmark(recall, true)
+
+                    showSuccessMessage(context, R.string.message_recall_bookmarked)
+                }
+            }
+        }
+    }
+
+    private suspend fun showSuccessMessage(context: Context, @StringRes messageId: Int) {
+        withContext(Dispatchers.Main) {
+            context.toast(messageId)
+        }
+    }
+}
--- app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/receiver/NotificationActionReceiver.kt
+++ app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/receiver/NotificationActionReceiver.kt
@@ -2,19 +2,8 @@ package com.sonphil.canadarecallsandsafetyalerts.receiver
 
 import android.content.Context
 import android.content.Intent
-import androidx.annotation.StringRes
-import com.sonphil.canadarecallsandsafetyalerts.R
-import com.sonphil.canadarecallsandsafetyalerts.ext.toast
 import com.sonphil.canadarecallsandsafetyalerts.presentation.recall.details.RecallDetailsActivityArgs
-import com.sonphil.canadarecallsandsafetyalerts.repository.BookmarkRepository
-import com.sonphil.canadarecallsandsafetyalerts.repository.ReadStatusRepository
-import com.sonphil.canadarecallsandsafetyalerts.repository.RecallRepository
-import com.sonphil.canadarecallsandsafetyalerts.utils.NotificationsUtils
 import dagger.android.DaggerBroadcastReceiver
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 /**
@@ -23,13 +12,7 @@ import javax.inject.Inject
 
 class NotificationActionReceiver : DaggerBroadcastReceiver() {
     @Inject
-    lateinit var notificationUtils: NotificationsUtils
-    @Inject
-    lateinit var recallRepository: RecallRepository
-    @Inject
-    lateinit var bookmarkRepository: BookmarkRepository
-    @Inject
-    lateinit var readStatusRepository: ReadStatusRepository
+    lateinit var notificationActionHandler: NotificationActionHandler
 
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
@@ -40,26 +23,6 @@ class NotificationActionReceiver : DaggerBroadcastReceiver() {
             RecallDetailsActivityArgs.fromBundle(this).recall
         }
 
-        if (recall != null) {
-            notificationUtils.dismissNotification(context, recall)
-
-            GlobalScope.launch(Dispatchers.IO) {
-                if (action == context.getString(R.string.action_mark_recall_as_read)) {
-                    readStatusRepository.markRecallAsRead(recall)
-
-                    showSuccessMessage(context, R.string.message_recall_marked_as_read)
-                } else if (action == context.getString(R.string.action_bookmark_recall)) {
-                    bookmarkRepository.updateBookmark(recall, true)
-
-                    showSuccessMessage(context, R.string.message_recall_bookmarked)
-                }
-            }
-        }
-    }
-
-    private suspend fun showSuccessMessage(context: Context, @StringRes messageId: Int) {
-        withContext(Dispatchers.Main) {
-            context.toast(messageId)
-        }
+        notificationActionHandler.handleRecallAction(context, recall, action)
     }
 }
