e56bcad673a945d9ef34d09e6a6f6be16135aad5
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2020-03-10 23:15:43 -0400

    Extract code from SyncRecallsWorker to GetNewRecallsUseCase, GetNotificationKeywordsUse, CheckIfShouldNotifyAboutRecallUseCase ans GetRecallsToNotifyUseCase

--- app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/worker/SyncRecallsWorker.kt
+++ app/src/main/java/com/sonphil/canadarecallsandsafetyalerts/worker/SyncRecallsWorker.kt
@@ -2,12 +2,8 @@ package com.sonphil.canadarecallsandsafetyalerts.worker
 
 import android.content.Context
 import androidx.work.*
-import com.sonphil.canadarecallsandsafetyalerts.data.entity.Recall
-import com.sonphil.canadarecallsandsafetyalerts.data.repository.NotificationKeywordsRepository
-import com.sonphil.canadarecallsandsafetyalerts.data.repository.RecallRepository
-import com.sonphil.canadarecallsandsafetyalerts.utils.LocaleUtils
+import com.sonphil.canadarecallsandsafetyalerts.domain.GetRecallsToNotifyUseCase
 import com.sonphil.canadarecallsandsafetyalerts.utils.NotificationsUtils
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.withContext
@@ -21,24 +17,26 @@ import javax.inject.Provider
 class SyncRecallsWorker @Inject constructor(
     appContext: Context,
     workerParameters: WorkerParameters,
-    private val recallRepository: RecallRepository,
-    private val notificationKeywordsRepository: NotificationKeywordsRepository,
-    private val localeUtils: LocaleUtils,
+    private val getRecallsToNotifyUseCase: GetRecallsToNotifyUseCase,
     private val notificationsUtils: NotificationsUtils
 ) : CoroutineWorker(appContext, workerParameters) {
 
     override suspend fun doWork(): Result = withContext(Dispatchers.Default) {
         try {
-            val lang = localeUtils.getCurrentLanguage()
-            val newRecalls = withContext(Dispatchers.IO) {
-                recallRepository.getNewRecalls(lang)
-            }
             val keywordNotificationsEnabled = inputData.getBoolean(
                 SyncRecallsWorkerScheduler.KEY_KEYWORD_NOTIFICATIONS_ENABLED,
                 false
             )
 
-            newRecalls.notify(this, keywordNotificationsEnabled)
+            run loop@{
+                getRecallsToNotifyUseCase(keywordNotificationsEnabled).forEach { recall ->
+                    if (isActive) {
+                        notificationsUtils.notifyRecall(recall)
+                    } else {
+                        return@loop
+                    }
+                }
+            }
 
             Result.success()
         } catch (e: Exception) {
@@ -46,60 +44,15 @@ class SyncRecallsWorker @Inject constructor(
         }
     }
 
-    private fun shouldNotifyAboutRecall(
-        recall: Recall,
-        keywordNotificationsEnabled: Boolean,
-        keywords: List<String>
-    ): Boolean {
-        if (!keywordNotificationsEnabled || keywords.isEmpty()) {
-            return true
-        }
-
-        val firstMatch = recall
-            .title
-            ?.findAnyOf(keywords, ignoreCase = true)
-
-        return firstMatch != null
-    }
-
-    private suspend fun List<Recall>.notify(
-        coroutineScope: CoroutineScope,
-        keywordNotificationsEnabled: Boolean
-    ) {
-        if (this.isNotEmpty()) {
-            val keywords = if (keywordNotificationsEnabled) {
-                notificationKeywordsRepository.getKeywords()
-            } else {
-                emptyList()
-            }
-
-            run loop@{
-                this.forEach { recall ->
-                    if (coroutineScope.isActive) {
-                        if (shouldNotifyAboutRecall(recall, keywordNotificationsEnabled, keywords)) {
-                            notificationsUtils.notifyRecall(recall)
-                        }
-                    } else {
-                        return@loop
-                    }
-                }
-            }
-        }
-    }
-
     class Factory @Inject constructor(
-        private val recallRepository: Provider<RecallRepository>,
-        private val notificationKeywordsRepository: Provider<NotificationKeywordsRepository>,
-        private val localeUtils: Provider<LocaleUtils>,
+        private val recallsToNotifyUseCase: Provider<GetRecallsToNotifyUseCase>,
         private val notificationsUtils: Provider<NotificationsUtils>
     ) : ChildWorkerFactory {
         override fun create(appContext: Context, params: WorkerParameters): ListenableWorker {
             return SyncRecallsWorker(
                 appContext,
                 params,
-                recallRepository.get(),
-                notificationKeywordsRepository.get(),
-                localeUtils.get(),
+                recallsToNotifyUseCase.get(),
                 notificationsUtils.get()
             )
         }
