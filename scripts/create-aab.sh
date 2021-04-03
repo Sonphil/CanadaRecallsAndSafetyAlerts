5d6181758582582ecc1938279d62997bb312ee74
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2021-04-03 15:43:58 -0400

    Add Google Play service account file and udpate CI to upload mapping file

--- scripts/create-aab.sh
+++ scripts/create-aab.sh
@@ -16,11 +16,20 @@ github::set_output() {
 
 ./gradlew bundleRelease
 
-readonly path="$(find app/build/outputs -name '*.aab' | head -1)"
+readonly aab_path="$(find app/build/outputs -name '*.aab' | head -1)"
 
-if [[ -f "$path" ]]; then
-    github::debug "found an aab file at $path"
-    github::set_output "path" "$path"
+if [[ -f "$aab_path" ]]; then
+    github::debug "found an aab file at $aab_path"
+    github::set_output "aab_path" "$aab_path"
 else
     github::debug "could not find an aab file"
+fi
+
+readonly mapping_file_path="$(find app/build/outputs -name 'mapping.txt' | head -1)"
+
+if [[ -f "$mapping_file_path" ]]; then
+    github::debug "found an mapping file at $mapping_file_path"
+    github::set_output "mapping_file_path" "$mapping_file_path"
+else
+    github::debug "could not find an mapping file"
 fi
--- scripts/decrypt_secrets.sh
+++ scripts/decrypt_secrets.sh
@@ -18,6 +18,11 @@ if [[ -z "${KEYSTORE_ENC_PW:-}" ]]; then
     github::failure
 fi
 
+if [[ -z "${GOOGLE_PLAY_SERVICE_ACCOUNT_ENC_PW:-}" ]]; then
+    github::error "GOOGLE_PLAY_SERVICE_ACCOUNT_ENC_PW is required"
+    github::failure
+fi
+
 readonly working_directory="$(git rev-parse --show-toplevel)"
 
 cd "$working_directory"
@@ -28,3 +33,4 @@ openssl version
 openssl aes-256-cbc -salt -a -d -k "$GOOGLE_SERVICES_ENC_PW" -in ./encrypted/google-services.json.enc -out ./app/google-services.json
 openssl aes-256-cbc -salt -a -d -k "$KEYSTORE_ENC_PW" -in ./encrypted/sonphil_keystore.jks.enc -out ./sonphil_keystore.jks
 openssl aes-256-cbc -salt -a -d -k "$KEYSTORE_ENC_PW" -in ./encrypted/keystore.properties.enc -out ./keystore.properties
+openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -k "$GOOGLE_PLAY_SERVICE_ACCOUNT_ENC_PW" -in ./encrypted/google-play-service-account.json.enc -out ./google-play-service-account.json
