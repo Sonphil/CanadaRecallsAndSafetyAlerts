ee56705cf618500e84209283e359cd4fe142e9e1
Thanh-Son-Philippe Lam <t.s.philippe_lam@hotmail.com>
2021-09-11 12:35:29 -0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,19 @@ allprojects {
     }
 }
 
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    fun isNonStable(version: String): Boolean {
+        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+        val isStable = stableKeyword || regex.matches(version)
+        return isStable.not()
+    }
+
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
